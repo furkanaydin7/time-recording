1141ba3f591bf76967286e2b4ee16a274d0f71cf
philipp.dunkel <philipp.dunkel@students.fhnw.ch>
2025-05-12 23:14:37 +0200

    User.java Entitätsklasse erstellt UserStatus.java Enum Klasse erstellt

---
+++ src/main/java/model/enums/UserStatus.java
@@ -0,0 +1,10 @@
+package model.enums;
+
+/**
+ * Status für Benutzer
+ * @author PD
+ * Code von anderen Teammitgliedern oder Quellen wird durch einzelne Kommentare deklariert
+ */
+public class UserStatus {
+    ACTIVE, INACTIVE, LOCKED, VACATION, PASSWORD_RESET_REQUIRED
+}
